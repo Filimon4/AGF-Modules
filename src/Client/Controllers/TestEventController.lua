98987f5b91a0bb42899e56efa37230ce216a3712
FILIMON <f.rykov@bk.ru>
2022-07-11 16:51:31 +0300

    Test of RemoteFunction Clinet/Server (Server/Client is not allow)

--- src/Client/Controllers/TestEventController.lua
+++ src/Client/Controllers/TestEventController.lua
@@ -7,13 +7,14 @@
 local TestEventController = {}
 
 function TestEventController:Say(mes)
-    print("Hello, "..mes.."!")
+    -- print("Hello, "..mes.."!")
 end
 
 function TestEventController:Start()
 	self.Controllers.TestController:ConnectEvent('Test', function(mes)
-        print(mes)        
+        -- print(mes)        
     end)
+    self.Services.MyService:SendMessage('Hello from the client')
 end
 
 
--- src/Server/Services/MyService.lua
+++ src/Server/Services/MyService.lua
@@ -12,11 +12,15 @@ function MyService:SayHello(name)
     print("Helloy, "..name.."!")
 end
 
+function MyService.Client:SendMessage(player, message)
+    print(player.Name .. " says: \""..message.."\"")
+end
+
 function MyService:Start()
-    for i = 1,10 do
-        wait(0.1)
-        self:FireEvent(EVENT_HELLO, "Hello "..i)
-    end
+    -- for i = 1,10 do
+    --     wait(0.1)
+    --     self:FireEvent(EVENT_HELLO, "Hello "..i)
+    -- end
 end
 
 
