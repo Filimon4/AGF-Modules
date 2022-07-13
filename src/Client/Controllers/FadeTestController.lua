036cd6f50a1e17bead407188f1e5f9fa43094264
FILIMON <f.rykov@bk.ru>
2022-07-13 12:31:27 +0300

    test of fade:to and fade:fromto

--- src/Client/Controllers/FadeTestController.lua
+++ src/Client/Controllers/FadeTestController.lua
@@ -6,15 +6,32 @@
 
 local FadeTestController = {}
 
+function FadeTestController:In_Out(Fade)
+    print("Fade In")
+	Fade:Out(2, true) -- true is mean that script doesn't wait for end of animation
+    task.wait(2)
+    print('Fade Out')
+    Fade:In(0.5) -- false is mean that script does wait for end of animation
+end
+
+function FadeTestController:To_FromTo(Fade)
+    print('Fade To')
+    print('75%')
+    Fade:To(0.75)
+    task.wait(3)
+    print('From 75% to 100%')
+    Fade:FromTo(0.75,0)
+    task.wait(3)
+    print('From 100% to 0%')
+    Fade:FromTo(0,1)
+end
 
 function FadeTestController:Start()
     print('FadeController started')
+    local Fade = self.Controllers.Fade
     task.wait(10)
-    print("Fae In")
-	self.Controllers.Fade:Out(2, true) -- true is mean that script doesn't wait for end of animation
-    task.wait(2)
-    print('Fade Out')
-    self.Controllers.Fade:In(0.5) -- false is mean that script does wait for end of animation
+    --self:In_Out(Fade)
+    self:To_FromTo(Fade)
 end
 
 return FadeTestController
