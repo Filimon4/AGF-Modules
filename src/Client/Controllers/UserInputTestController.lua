f0c1578b734514d98644ca5429d680cda7db2dc5
FILIMON <f.rykov@bk.ru>
2022-07-15 14:05:32 +0300

    minor

--- src/Client/Controllers/UserInputTestController.lua
+++ src/Client/Controllers/UserInputTestController.lua
@@ -62,7 +62,7 @@ function UserInputTestController:MouseMethods(mouse)
     --self:LockInCenter(mouse)
     --self:Raycast(mouse)
     --self:RaycastFromXY(mouse)
-    self:RaycastWithParams(mouse) -- in documentation there is methods Cast,CastWithIgnoreList and CastWithWhitelist. I will use Raycast methode, bsc it's just simpler
+    --self:RaycastWithParams(mouse) -- in documentation there is methods Cast,CastWithIgnoreList and CastWithWhitelist. I will use Raycast methode, bsc it's just simpler
 
     -- //
 end
